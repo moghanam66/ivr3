05293408187c0436270bee08f08a023869660f2b
Mohamed Elghannam <82755084+moghanam66@users.noreply.github.com>
2025-03-02 01:23:31 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ import os
 class DefaultConfig:
     """Bot Configuration"""
 
-    PORT = 3978
+    PORT = 8000
     APP_ID = os.environ.get("b0a29017-ea3f-4697-aef7-0cb05979d16c", "")
     APP_PASSWORD = os.environ.get("2fc8Q~YUZMbD8E7hEb4.vQoDFortq3Tvt~CLCcEQ", "")
